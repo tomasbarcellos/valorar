292be9b15cc86f1e51169a5d8ad7f82407d63b4a
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-10-26 18:17:00 -0200

    corrige problema para login no site do valor

--- R/login.R
+++ R/login.R
@@ -9,19 +9,21 @@
 #' @examples
 #'
 login <- function(email, senha) {
-  url <- 'http://www.valor.com.br/login?'
+  url <- 'http://www.valor.com.br/login'
   sessao <- html_session(url)
   indice_login <- sessao %>% html_nodes('form') %>% html_attr('id') %>%
     `==`('user-login') %>% which()
   formulario <- html_form(sessao)[[indice_login]]
   preenchido <- set_values(formulario, mail = email, pass = senha)
   submetido <- submit_form(sessao, preenchido)
-  confirmacao <- submetido %>% html_nodes('ul') %>% html_text()
-  if (any(grepl('Login', confirmacao))) {
-    warning('Houve falha no login\n', call. = FALSE)
-  } else {
+  submetido <- jump_to(submetido, submetido$url) %>% follow_link('Login')
+
+  if (any(grepl('/[0-9]{8}/', submetido$url))) { #codigo de usuario no link
     message('Login realizado com sucesso')
     print(confirmacao)
+  } else {
+    warning('Houve falha no login\n', call. = FALSE)
   }
+
   submetido
 }
