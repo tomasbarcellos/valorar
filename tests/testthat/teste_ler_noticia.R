ca2dcba9de65a638216bbd4c816078b80a1369be
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-11-02 18:43:41 -0200

    Transforma noticia em tibble e altera método de impressao
    Também inclui caso com erro no teste

--- tests/testthat/teste_ler_noticia.R
+++ tests/testthat/teste_ler_noticia.R
@@ -5,7 +5,15 @@ links <- sessao %>% read_html() %>% html_nodes('.teaser-title a') %>%
   html_attr('href') %>%  `[`(. != '') %>%
   ifelse(test = grepl(pattern = '\\bhttp', x = .), no = paste0('http://www.valor.com.br', .))
 
-noticia1 <- ler_noticia(sessao, links[1])
+valor_investe <- 'http://www.valor.com.br/u/5179516'
+
+test_that('leituras ocorrem sem erro', {
+  expect_silent(
+    noticia1 <<- ler_noticia(sessao, links[1])
+  )
+  expect_error(ler_noticia(sessao, valor_investe), 'xml_missing')
+})
+
 
 test_that('Meta informações da resposta', {
   expect_s3_class(noticia1, 'noticia')
