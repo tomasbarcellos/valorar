1e5e99ca7e4337e5e34d1fe547116ef657eca023
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-11-02 20:51:56 -0200

    Inclui teste para procurar_valor()

--- tests/testthat/teste_procurar_valor.R
+++ tests/testthat/teste_procurar_valor.R
@@ -1,11 +1,11 @@
 context('procurar_valor()')
 
 test_that('Retorna sessao com resultado da busca', {
-  if (.Platform$OS.type == 'windows') {
-    expect_s3_class((busca <- procurar_valor('desemprego')), 'session')
+  # if (.Platform$OS.type == 'windows') {
+    expect_s3_class((busca <<- procurar_valor('desemprego')), 'session')
     expect_false(is.null(attr(busca, 'resultados')))
     expect_false(is.null(attr(busca, 'pagina')))
-  } else {
-    expect_true(TRUE)
-  }
+  # } else {
+  #   expect_true(TRUE)
+  # }
 })
