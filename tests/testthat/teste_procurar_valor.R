299b21b004a4f7a5f46449c945c24e478d9d78cc
tomasbarcellos <tomasbarcellos@gmail.com>
2017-11-02 16:31:55 -0200

    descomenta testes para phantomJS, mas não rodarão no servidor

--- tests/testthat/teste_procurar_valor.R
+++ tests/testthat/teste_procurar_valor.R
@@ -1,7 +1,11 @@
 context('procurar_valor()')
 
 test_that('Retorna sessao com resultado da busca', {
-  # expect_s3_class((busca <- procurar_valor('desemprego')), 'session')
-  # expect_false(is.null(attr(busca, 'resultados')))
-  # expect_false(is.null(attr(busca, 'pagina')))
+  if (.Platform$OS.type == 'windows') {
+    expect_s3_class((busca <- procurar_valor('desemprego')), 'session')
+    expect_false(is.null(attr(busca, 'resultados')))
+    expect_false(is.null(attr(busca, 'pagina')))
+  } else {
+    expect_true(TRUE)
+  }
 })
