5dfa1344dcc58ea96075536f98e465b0129a9554
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-11-02 22:26:09 -0200

    Apaga comentarios nos testes do phantomJS

--- tests/testthat/teste_procurar_valor.R
+++ tests/testthat/teste_procurar_valor.R
@@ -1,11 +1,7 @@
 context('procurar_valor()')
 
 test_that('Retorna sessao com resultado da busca', {
-  # if (.Platform$OS.type == 'windows') {
     expect_s3_class((busca <<- procurar_valor('desemprego')), 'session')
     expect_false(is.null(attr(busca, 'resultados')))
     expect_false(is.null(attr(busca, 'pagina')))
-  # } else {
-  #   expect_true(TRUE)
-  # }
 })
