b6ed2f7a42221bf4dc5f2aa4539bbf41fe146292
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-11-03 16:17:32 -0200

    inclui documentação da função links_pagina()

--- R/links_pagina.R
+++ R/links_pagina.R
@@ -1,3 +1,12 @@
+#' Pega noticias da sessao
+#'
+#' @param sessao Uma sessao criada por \code{\link{html_session}}
+#' @param css Seletor de css que será passado para \code{\link{html_nodes}}
+#'
+#' @return Uma string com os links da pagina no css
+#' @export
+#'
+#' @examples
 links_pagina <- function(sessao = html_session('http://www.valor.com.br'), css = '.teaser-title a') {
   sessao %>% read_html() %>% html_nodes(css) %>% html_attr('href') %>%
     magrittr::extract(. != '') %>%
