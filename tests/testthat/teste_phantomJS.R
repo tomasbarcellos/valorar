c61f8784957c7d77894d1cd1f2a55bec74f1d4d3
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2018-09-08 21:46:20 -0300

    Remove arquivos relacionados a antiga arquitetura

--- tests/testthat/teste_phantomJS.R
+++
@@ -1,6 +0,0 @@
-# context('phantomJS()')
-#
-# test_that('Retorna resultado esperado', {
-#     expect_silent(resposta <- phantomJS())
-#     expect_named(resposta, c('version', 'dir', 'path'))
-# })
